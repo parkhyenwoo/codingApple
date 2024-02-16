2524c970d436fa6083d7ed7383a1de6e6361b91e
parkhyenwoo <hyenwoo05@gmail.com>
2024-02-16 17:13:17 +0900

    모달창 인풋박스추가

--- src/Modal.vue
+++ src/Modal.vue
@@ -4,7 +4,8 @@
       <h4>{{ oneroom[index].title }}</h4>
       <img :src="oneroom[index].image" style="width: 100%" />
       <p>{{ oneroom[index].content }}</p>
-      <p>{{ oneroom[index].price }}</p>
+      <input type="range" min="0" max="12" v-model="month" />
+      <p>{{ month }}개월 선택함 {{ oneroom[index].price * month }}</p>
     </div>
     <disCount></disCount>
     <button @click="$emit('modalClose')">창 닫기</button>
@@ -13,6 +14,21 @@
 <script>
 export default {
   name: "customModal",
+  data() {
+    return {
+      month: 1,
+    };
+  },
+  watch: {
+    month(m) {
+      if (m >= 13) {
+        alert("13이상 입력하지마셈");
+      } else if (isNaN(m) == true) {
+        alert("문자열 입력하지마라");
+        this.month = 1;
+      }
+    },
+  },
   props: {
     oneroom: Object,
     index: Number,
